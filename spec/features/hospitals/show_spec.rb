692c5149e68cb022ef1bd2e74b453daf952323b0
Andrew Speth <speth.andrew@yahoo.com>
2022-04-25 09:48:57 -0600

    feature and model tests for user story 2

---
+++ spec/features/hospitals/show_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe "hospital show page" do
+  it "has the hospital name, all the doctors who work there, and a unique list of universities that this hospital's doctors attended" do
+    hospital_1 = Hospital.create!(name: "Rose Medical")
+    hospital_2 = Hospital.create!(name: "Fake Hospital")
+    doc_1 = hospital_1.doctors.create!(name: "Dr. Speth", specialty: "Optometry", university: "SCCO")
+    doc_2 = hospital_1.doctors.create!(name: "Dr. Dog", specialty: "Music", university: "The Streets")
+    doc_3 = hospital_1.doctors.create!(name: "Dr. LeGate", specialty: "Optometry", university: "SCCO")
+    doc_4 = hospital_1.doctors.create!(name: "Dr. Smith", specialty: "Chiropractic", university: "SCUHS")
+    doc_5 = hospital_2.doctors.create!(name: "Dr. Jan Itor", specialty: "Custodial Sciences", university: "Sacred Heart")
+
+    visit "/hospitals/#{hospital_1.id}"
+
+    within "#doctors" do
+      expect(page).to have_content("Name: Dr. Speth")
+      expect(page).to have_content("Name: Dr. Dog")
+      expect(page).to have_content("Name: Dr. LeGate")
+      expect(page).to have_content("Name: Dr. Smith")
+      expect(page).to_not have_content("Name: Dr. Jan Itor")
+    end
+
+    within "#universities" do
+      expect(page).to have_content("SCCO")
+      expect(page).to have_content("The Streets")
+      expect(page).to have_content("SCUHS")
+      expect(page).to_not have_content("Sacred Heart")
+    end
+  end
+end
+
+# As a visitor
+# When I visit a hospital's show page
+# I see the hospital's name
+# And I see the number of doctors that work at this hospital
+# And I see a unique list of universities that this hospital's doctors attended
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe "instance methods" do
+    it "#unique_universities" do
+    hospital_1 = Hospital.create!(name: "Rose Medical")
+    hospital_2 = Hospital.create!(name: "Fake Hospital")
+    doc_1 = hospital_1.doctors.create!(name: "Dr. Speth", specialty: "Optometry", university: "SCCO")
+    doc_2 = hospital_1.doctors.create!(name: "Dr. Dog", specialty: "Music", university: "The Streets")
+    doc_3 = hospital_1.doctors.create!(name: "Dr. LeGate", specialty: "Optometry", university: "SCCO")
+    doc_4 = hospital_1.doctors.create!(name: "Dr. Smith", specialty: "Chiropractic", university: "SCUHS")
+    doc_5 = hospital_2.doctors.create!(name: "Dr. Jan Itor", specialty: "Custodial Sciences", university: "Sacred Heart")
+
+    expect(hospital_1.unique_universities).to eq(["SCCO, The Streets, SCUHS"])
+    end
+  end
 end
