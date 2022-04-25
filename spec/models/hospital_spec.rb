ee95b175b0b5b5d9db9cffab65a3d4cdff1fa616
Sherman Au <shermau13@gmail.com>
2022-04-25 10:00:18 -0600

    test: HospitalModel validations and relationships

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Hospital do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+  end
+
   describe 'relationships' do
     it { should have_many(:doctors) }
+    it { should have_many(:doctor_patients) }
+    it { should have_many(:patients) }
   end
 end
