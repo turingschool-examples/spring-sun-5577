78c543289d0d37750531f8b7fee30b30a838c208
Chris Romero <chrisleeromero1@gmail.com>
2022-04-25 11:17:56 -0600

    feat: add instance method for unique list of university names

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe 'instance methods' do
+    it 'returns a unique list of university names' do
+      hospital = Hospital.create!(name: 'UC Denver')
+      hospital_2 = Hospital.create!(name: 'SLC')
+      doctor_1 = hospital.doctors.create!(name: 'Chris', specialty: 'ER', university: 'UT')
+      doctor_2 = hospital.doctors.create!(name: 'Sophie', specialty: 'PCP', university: 'Harvard')
+      doctor_3 = hospital.doctors.create!(name: 'Sam', specialty: 'PCP', university: 'Harvard')
+      doctor_4 = hospital.doctors.create!(name: 'Jason', specialty: 'PCP', university: 'Stanford')
+      doctor_5 = hospital.doctors.create!(name: 'Juan', specialty: 'PCP', university: 'Stanford')
+      doctor_6 = hospital_2.doctors.create!(name: 'Dave', specialty: 'PCP', university: 'Cornell')
+      expect(hospital.university_list).to eq(["Harvard", "Stanford", "UT"])
+    end
+  end
 end
