5395eb90f6010e07e77186b43b15cb933a030514
Sherman Au <shermau13@gmail.com>
2022-04-25 09:49:23 -0600

    feat: patient relationships

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < ApplicationRecord
   validates :name, presence: true
   validates :age,  presence: true
+
+  has_many :doctor_patients
+  has_many :doctors, through: :doctor_patients
 end
