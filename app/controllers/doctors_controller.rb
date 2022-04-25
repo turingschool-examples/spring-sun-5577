cceba86c8778a36b644bd966654953868f2e9b3d
Luke Pascale <lpluke5@gmail.com>
2022-04-25 12:44:58 -0500

    End of Story 3

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -2,4 +2,11 @@ class DoctorsController < ApplicationController
   def show
     @doctor = Doctor.find(params[:id])
   end
+
+  def destroy
+    @patient = Patient.find(params[:id])
+    doc_id = @patient.doctor_ids
+    @patient.destroy
+    redirect_to "/doctors/#{doc_id}/"
+  end
 end
