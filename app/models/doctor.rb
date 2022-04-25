21660f7c2207db06f8165b2315e69e985cc1e34a
Luke Pascale <lpluke5@gmail.com>
2022-04-25 10:34:34 -0500

    ModelFeatTest: Setup validations and relationships and initialize controllers

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,8 @@
 class Doctor < ApplicationRecord
   belongs_to :hospital
+  has_many :doctor_patients
+
+  validates_presence_of :name
+  validates_presence_of :specialty
+  validates_presence_of :university
 end
--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 RSpec.describe Doctor do
   describe 'relationships' do
     it { should belong_to(:hospital) }
-    it { should have_many(:patients).through(:doctor_patients)}
   end
 
   describe 'validations' do
