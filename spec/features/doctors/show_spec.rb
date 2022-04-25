3affed465fe95d99a7bd607c203603ac4f354b5e
Drew Proebstel <d.proebstel@gmail.com>
2022-04-25 08:51:27 -0700

    doctor show spec

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -2,15 +2,43 @@ require 'rails_helper'
 
 RSpec.describe 'the doctor show page' do
    it 'shows all the attributes for a doctor' do
-
-        merchant = Merchant.create(name: "Braum's")
-        item1 = merchant.items.create(name: "Toast", description: "Let it rip!", unit_price: 1000)
-        bob = Customer.create!(first_name: "Bob", last_name: "Benson")
-        invoice_1 = bob.invoices.create!(status: 1, created_at: '05 Apr 2022 00:53:36 UTC +00:00')
-        visit "/merchants/#{merchant.id}/invoices/#{invoice_1.id}"
-        expect(page).to have_content("#{invoice_1.id}")
-        expect(page).to have_content("completed")
-        expect(page).to have_content("Bob Benson")
-        expect(page).to have_content('Tuesday, April 05, 2022')
+      hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      mcdreamy = hospital.doctors.create!(name: "Patrick Dempsy", specialty: "Brains", university: "Oregon State University")
+        visit "doctors/#{bailey.id}"
+        expect(page).to have_content("Miranda Bailey")
+        expect(page).to have_content("General Surgery")
+        expect(page).to have_content("Stanford University")
+        expect(page).to_not have_content("Patrick Dempsy")
+        expect(page).to_not have_content("Brains")
+        expect(page).to_not have_content("Oregon State University")
+    end
+   it 'shows all the patients for a doctor' do
+      hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      mcdreamy = hospital.doctors.create!(name: "Patrick Dempsy", specialty: "Brains", university: "Oregon State University")
+      patient1 = bailey.patients.create!(name:"Drew", age:27)
+      patient2 = bailey.patients.create!(name:"Daver", age:37)
+      patient3 = mcdreamy.patients.create!(name:"Patty", age:69)
+        visit "doctors/#{bailey.id}"
+        expect(page).to have_content("Drew")
+        expect(page).to have_content("Daver")
+        expect(page).to have_content("27")
+        expect(page).to_not have_content("37")
+        expect(page).to_not have_content("Patty")
+        expect(page).to_not have_content("69")
+    end
+   it 'shows name of hospitol for a doctor' do
+      hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      hospital2 = Hospital.create!(name: "Turing Hospital")
+      timmy = hospital2.doctors.create!(name: "Timmy", specialty: "Butts", university: "Turing University")
+      bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      mcdreamy = hospital.doctors.create!(name: "Patrick Dempsy", specialty: "Brains", university: "Oregon State University")
+      patient1 = bailey.patients.create!(name:"Drew", age:27)
+      patient2 = bailey.patients.create!(name:"Daver", age:37)
+      patient3 = mcdreamy.patients.create!(name:"Patty", age:69)
+        visit "doctors/#{bailey.id}"
+        expect(page).to have_content("Grey Sloan Memorial Hospital")
+        expect(page).to_not have_content("Turing Hospital")
     end
   end
