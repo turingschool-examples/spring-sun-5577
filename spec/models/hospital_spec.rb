27f19fe74104d9d3ab1621e8cb1e21b0f7c57fa2
James Harkins <jh.harkins@protonmail.com>
2022-04-25 09:36:11 -0600

    ModelTest: create test for hospital model unique_doctor_universities instance method

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,22 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe 'instance methods' do
+    describe '.unique_doctor_universities' do
+      it "returns a distinct list of the universities where the hospitals doctors studied" do
+        hospital_1 = Hospital.create!(name: "Denver Hospital")
+        hospital_2 = Hospital.create!(name: "DC Hospital")
+        doctor_1 = hospital_1.doctors.create!(name: "Something Kennedy", specialty: "Cancer", university: "VCU")
+        doctor_2 = hospital_1.doctors.create!(name: "Bill Notadoctor", specialty: "Gastrointestinal", university: "NYU")
+        doctor_3 = hospital_1.doctors.create!(name: "Brian Definitelynotadoctor", specialty: "Sports injuries", university: "Johns Hopkins")
+        doctor_4 = hospital_1.doctors.create!(name: "Dr. Strangelove", specialty: "Nuclear Winter", university: "Johns Hopkins")
+        doctor_5 = hospital_1.doctors.create!(name: "Dr. Evil", specialty: "Evil", university: "VCU")
+        doctor_5 = hospital_2.doctors.create!(name: "Another Doctor", specialty: "Doctoring", university: "Harvard")
+        doctor_5 = hospital_2.doctors.create!(name: "YetAnother Doctor", specialty: "More specific doctoring", university: "Yale")
+
+        expect(hospital_1.unique_doctor_universities).to eq(["VCU", "NYU", "Johns Hopkins"])
+      end
+    end
+  end
 end
