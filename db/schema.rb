61300ab99264437b708b6dab2c3f13ba6e5ab523
Alex Psitos <apsitos08@gmail.com>
2022-04-25 11:30:49 -0400

    Fix migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_25_151901) do
+ActiveRecord::Schema.define(version: 2022_04_25_152048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "doctor_patients", force: :cascade do |t|
-    t.bigint "doctors_id"
-    t.bigint "patients_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["doctors_id"], name: "index_doctor_patients_on_doctors_id"
-    t.index ["patients_id"], name: "index_doctor_patients_on_patients_id"
+    t.bigint "doctor_id"
+    t.bigint "patient_id"
+    t.index ["doctor_id"], name: "index_doctor_patients_on_doctor_id"
+    t.index ["patient_id"], name: "index_doctor_patients_on_patient_id"
   end
 
   create_table "doctors", force: :cascade do |t|
@@ -47,7 +45,7 @@ ActiveRecord::Schema.define(version: 2022_04_25_151901) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "doctor_patients", "doctors", column: "doctors_id"
-  add_foreign_key "doctor_patients", "patients", column: "patients_id"
+  add_foreign_key "doctor_patients", "doctors"
+  add_foreign_key "doctor_patients", "patients"
   add_foreign_key "doctors", "hospitals"
 end
