94c81e335f582c2f90aa3e1a4bd057fd07c9fbdf
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-25 11:09:51 -0600

    Feat: add route, controller, and view for patients index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   resources :hospitals, only: [:show]
   resources :doctors, only: [:show, :destroy]
-  
+
+  get '/patients', to: 'patients#index'
   # get '/doctors/:id', to: 'doctors#show'
   # delete '/doctors/:id', to: 'doctors#destroy'
 
