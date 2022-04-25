d933ac949b74153084ace69e03a4d07a2d90dadc
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-25 11:41:41 -0600

    Feat: add patient_name instance method to doc-patient model

--- app/models/doctor_patient.rb
+++ app/models/doctor_patient.rb
@@ -2,4 +2,9 @@ class DoctorPatient < ApplicationRecord
   belongs_to :doctor
   belongs_to :patient
 
+
+  def patient_name
+    patient.name
+  end
+  
 end 
--- spec/models/doctor_patient_spec.rb
+++ spec/models/doctor_patient_spec.rb
@@ -6,4 +6,23 @@ RSpec.describe DoctorPatient, type: :model do
     it { should belong_to :patient }
   end
 
+  describe 'instance methods' do
+
+    it 'returns the name of a patient associated with a doctor_patient' do 
+      hos1 = Hospital.create!(name: 'Big Hospital')
+      doc1 = Doctor.create!(name: 'Barry Bonds', specialty: "Steroids", university: "San Fransciso State", hospital_id: hos1.id)
+
+      pat1 = Patient.create!(name: 'Skeeter', age: 35)
+      pat2 = Patient.create!(name: 'LugNut', age: 23)
+      pat3 = Patient.create!(name: 'Ruby', age: 44)
+
+      #doc1 patients 
+      docpat1 = DoctorPatient.create!(doctor_id: doc1.id, patient_id: pat1.id)
+      docpat2 = DoctorPatient.create!(doctor_id: doc1.id, patient_id: pat2.id)
+      docpat3 = DoctorPatient.create!(doctor_id: doc1.id, patient_id: pat3.id)
+
+      expect(docpat1.patient_name).to eq('Skeeter')
+    end 
+  end 
+
 end 
