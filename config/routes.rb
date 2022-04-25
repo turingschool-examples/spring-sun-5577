0df8bab45be980359099f7a5b4f431a96714d631
Alex Psitos <apsitos08@gmail.com>
2022-04-25 12:54:28 -0400

    Feat: create route to remove doctor patients

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: :show
-
-  resources :hospitals, only: :show
+  resources :doctors, only: [:show]
+  resources :doctor_patients, only: [:destroy]
+  resources :hospitals, only: [:show]
 end
