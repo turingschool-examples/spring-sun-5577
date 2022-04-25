c98c71ffc61f10e217bfa3eab0fa71307ce2d16a
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-25 09:53:02 -0600

    Feat: Hospital show page displays name

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Doctors Show Page" do
   before :each do
     @cuddle_town = Hospital.create!(name: "Cuddle Town Emergency Center")
+    @broken_bones = Hospital.create!(name: "Break Your Bones Emergency Center")
 
     @doc_1 = @cuddle_town.doctors.create!(name: "Dr. Oopsies", specialty: "Messing Up", university: "Cute College")
     @doc_2 = @cuddle_town.doctors.create!(name: "Dr. Nono", specialty: "Fumbling Delicate Tools", university: "UPENN")
@@ -18,7 +19,7 @@ RSpec.describe "Doctors Show Page" do
 
   it "displays the doctor name, specialty, and university" do
     visit "/doctors/#{@doc_1.id}"
-    save_and_open_page
+
     expect(page).to have_content(@doc_1.name)
     expect(page).to have_content(@doc_1.specialty)
     expect(page).to have_content(@doc_1.university)
@@ -29,5 +30,6 @@ RSpec.describe "Doctors Show Page" do
 
     expect(page).to_not have_content(@doc_2.name)
     expect(page).to_not have_content(@dani.name)
+    expect(page).to_not have_content(@broken_bones.name)
   end
 end
