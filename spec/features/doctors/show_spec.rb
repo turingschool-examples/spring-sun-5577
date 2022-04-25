dd49f570699fa067d559d52d485179e2533b0cbf
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-25 10:34:54 -0600

    Test: Doctor show page has button to remove patient from doctor's caseload

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -38,4 +38,16 @@ RSpec.describe "Doctors Show Page" do
     expect(page).to_not have_content(@dani.name)
     expect(page).to_not have_content(@broken_bones.name)
   end
+
+  it "has a button to remove patient from doctor's caseload" do
+    visit "/doctors/#{@doc_1.id}"
+
+    expect(page).to have_content(@johnny.name)
+    
+    within("##{@johnny.id}") do
+      click_button("Remove Patient From Workload")
+    end
+
+    expect(page).to_not have_content(@johnny.name)
+  end
 end
