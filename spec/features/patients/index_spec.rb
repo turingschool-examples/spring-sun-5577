25d5be14613e215ec7cbc3120f8e762e1581edd2
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 13:03:15 -0400

    Test: Create patients index test.

---
+++ spec/features/patients/index_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe 'Patients Index Page' do
+
+  it 'Contains all patients in descending order of age' do
+    patient_1 = Patient.create(name: "Edge", age: 43)
+    patient_2 = Patient.create(name: "Kenny Omega", age: 36)
+    patient_3 = Patient.create(name: "Sting", age: 65)
+    patient_4 = Patient.create(name: "Kidd Bandit", age: 25)
+
+    visit patients_path()
+
+    patient_1_info = "#{patient_1.name}, Age #{patient_1.age}"
+    patient_2_info = "#{patient_2.name}, Age #{patient_2.age}"
+    patient_3_info = "#{patient_3.name}, Age #{patient_3.age}"
+    patient_1_info = "#{patient_4.name}, Age #{patient_4.age}"
+
+    within "#patients" do
+      expect(page).to have_content(patient_1_info)
+      expect(page).to have_content(patient_2_info)
+      expect(page).to have_content(patient_3_info)
+      expect(page).to have_content(patient_4_info)
+
+      expect(patient_3_info).to appear_before(patient_1_info)
+      expect(patient_3_info).to appear_before(patient_2_info)
+      expect(patient_3_info).to appear_before(patient_4_info)
+      expect(patient_1_info).to_not appear_before(patient_3_info)
+      expect(patient_1_info).to appear_before(patient_2_info)
+      expect(patient_1_info).to appear_before(patient_4_info)
+      expect(patient_2_info).to_not appear_before(patient_1_info)
+      expect(patient_2_info).to_not appear_before(patient_3_info)
+      expect(patient_2_info).to appear_before(patient_4_info)
+      expect(patient_4_info).to_not appear_before(patient_2_info)
+      expect(patient_4_info).to_not appear_before(patient_1_info)
+      expect(patient_4_info).to_not appear_before(patient_3_info)
+    end
+  end
+end
