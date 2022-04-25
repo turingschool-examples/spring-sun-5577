c976d225afbd2c9b7131f04e8fd1f981abc6adf6
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 11:05:33 -0400

    Initial commit: Create patient migrations.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_205253) do
+ActiveRecord::Schema.define(version: 2022_04_25_150455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,20 @@ ActiveRecord::Schema.define(version: 2021_06_14_205253) do
     t.index ["hospital_id"], name: "index_doctors_on_hospital_id"
   end
 
+  create_table "doctors_patients", id: false, force: :cascade do |t|
+    t.bigint "doctor_id", null: false
+    t.bigint "patient_id", null: false
+  end
+
   create_table "hospitals", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
