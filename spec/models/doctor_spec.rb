1c0128ed61d63bbfd9dbac9a3215ca726e79a38c
Andrew Speth <speth.andrew@yahoo.com>
2022-04-25 09:12:20 -0600

    relationship tests, migrations done

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Doctor do
   describe 'relationships' do
     it { should belong_to(:hospital) }
+    it { should have_many(:doctor_patients) }
+    it { should have_many(:patients).through(:doctor_patients) }
   end
 end
