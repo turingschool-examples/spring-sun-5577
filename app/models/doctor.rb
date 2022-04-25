fea851fa7479bbedf56913160ef7484a606fdfe9
Sandisz Thieme <sandiszthieme@gmail.com>
2022-04-25 09:28:19 -0600

    Test: all relationships

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,5 @@
 class Doctor < ApplicationRecord
   belongs_to :hospital
+  has_many :doctor_patients
+  has_many :patients, through: :doctor_patients
 end
--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe Doctor do
+RSpec.describe Patient do
   describe 'relationships' do
-    it { should belong_to(:hospital) }
     it { should have_many(:doctor_patients) }
-    it { should have_many(:patients) }
+    it { should have_many(:doctors) }
   end
 end
