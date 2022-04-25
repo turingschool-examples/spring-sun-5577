523c9e4f891fcb5f61cde467bc987e830e103144
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-25 11:35:16 -0600

    Test: feature and model tests for unique list of doctors universities

--- spec/features/hospitals/show_spec.rb
+++ spec/features/hospitals/show_spec.rb
@@ -17,4 +17,19 @@ RSpec.describe "Hospital Show Page" do
     expect(page).to have_content("Number of Doctors: 4")
     expect(page).to_not have_content("Denver General Hospital")
   end
+
+  it "displays a unique list of universities the doctors attended" do
+    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+    doctor1 = hospital1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+    doctor2 = hospital1.doctors.create!(name: "John Quincey", specialty: "Kidney Surgery", university: "Colorado University")
+    doctor3 = hospital1.doctors.create!(name: "Hawkeye Pierce", specialty: "Oral Surgery", university: "Arizona University")
+    doctor4 = hospital1.doctors.create!(name: "Charles Winchester", specialty: "Knee Surgery", university: "Stanford University")
+
+    visit hospital_path(hospital1.id)
+
+    within("#universities") do
+      expect(page).to have_content("These Doctors attended these Universities: Stanford University, Colorado University, Arizona University")
+    end
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -5,4 +5,17 @@ RSpec.describe Hospital, type: :model do
     it { should have_many(:doctors) }
     it { should have_many(:patients).through(:doctors) }
   end
+
+  describe "class methods", type: :model do
+    it "displays a unique list of universities the doctors attended" do
+      hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+
+      doctor1 = hospital1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      doctor2 = hospital1.doctors.create!(name: "John Quincey", specialty: "Kidney Surgery", university: "Colorado University")
+      doctor3 = hospital1.doctors.create!(name: "Hawkeye Pierce", specialty: "Oral Surgery", university: "Arizona University")
+      doctor4 = hospital1.doctors.create!(name: "Charles Winchester", specialty: "Knee Surgery", university: "Stanford University")
+
+      expect(hospital1.doctors_uniq_universities).to eq(["Stanford University", "Colorado University", "Arizona University"])
+    end
+  end
 end
