be5e784590a9773ff929d228b6eec8609f423679
Sherman Au <shermau13@gmail.com>
2022-04-25 09:23:14 -0600

    feat: migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_205253) do
+ActiveRecord::Schema.define(version: 2022_04_25_151823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctor_patients", force: :cascade do |t|
+    t.bigint "doctor_id"
+    t.bigint "patients_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_doctor_patients_on_doctor_id"
+    t.index ["patients_id"], name: "index_doctor_patients_on_patients_id"
+  end
 
   create_table "doctors", force: :cascade do |t|
     t.string "name"
@@ -31,5 +40,14 @@ ActiveRecord::Schema.define(version: 2021_06_14_205253) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "doctor_patients", "doctors"
+  add_foreign_key "doctor_patients", "patients", column: "patients_id"
   add_foreign_key "doctors", "hospitals"
 end
