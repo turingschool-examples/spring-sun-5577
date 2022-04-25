b78b3a05642d04891ee9c06d113d3218bbfa6170
Andrew Speth <speth.andrew@yahoo.com>
2022-04-25 11:34:12 -0600

    refactor DoctorPatients#destroy to use find_by, which returns an instance instead of association

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -2,8 +2,8 @@ class DoctorPatientsController < ApplicationController
   def destroy
     doctor = Doctor.find(params[:doctor_id])
     hospital = doctor.hospital
-    association = DoctorPatient.where(doctor_id: params[:doctor_id], patient_id: params[:patient_id])
-    association.first.destroy
+    doctor_patient = DoctorPatient.find_by(doctor_id: params[:doctor_id], patient_id: params[:patient_id])
+    doctor_patient.destroy
     redirect_to "/hospitals/#{hospital.id}/doctors/#{doctor.id}"
   end
 end
