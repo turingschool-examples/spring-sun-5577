82fc7302ee8a9a50f58bc16c1156852283820328
Will Medders <wmedders@gmail.com>
2022-04-25 13:01:54 -0400

    ModelTest: Patient #oldest_to_youngest

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe Patient do
     it { should have_many(:patient_doctors) }
     it { should have_many(:doctors).through(:patient_doctors) }
   end
+
+  describe 'class methods' do
+    it '#oldest_to_youngest' do
+      pat_1 = Patient.create!(name: 'Justin Hill', age: 39)
+      pat_2 = Patient.create!(name: 'Mark Gainey', age: 46)
+      pat_3 = Patient.create!(name: 'Liz Hagler', age: 35)
+      pat_4 = Patient.create!(name: 'Henry Mosley', age: 20)
+
+      expect(Patient.oldest_to_youngest).to eq([pat_2, pat_1, pat_3, pat_4])
+    end
+  end
 end
