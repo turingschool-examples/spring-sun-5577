d37889895ecae055e52d885ff31ac6d76eba13a9
Will Medders <wmedders@gmail.com>
2022-04-25 13:13:15 -0400

    Refactor hospital_spec #unique_univs expectation

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -14,14 +14,14 @@ RSpec.describe Hospital do
 
       expect(hosp_1.doctor_count).to eq(3)
     end
-    
+
     it '#unique_univs' do
       hosp_1 = Hospital.create!(name: 'City Health')
       doc_1 = hosp_1.doctors.create(name: 'Jim Pfeiffer', specialty: 'Pediatrics', university: 'Vanderbilt')
       doc_2 = hosp_1.doctors.create(name: 'Ted Leftwich', specialty: 'Proctology', university: 'Emory')
       doc_3 = hosp_1.doctors.create(name: 'Corbin Meikle', specialty: 'Brain Surgery', university: 'Emory')
 
-      expect(hosp_1.unique_univs.count).to eq(2)
+      expect(hosp_1.unique_univs).to eq(['Emory', 'Vanderbilt'])
     end
   end
 end
