4212fa912e569e3e792c57c3047fb09bda923246
Drew Proebstel <d.proebstel@gmail.com>
2022-04-25 09:53:47 -0700

    spec:story 3 doctor show page

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -38,4 +38,25 @@ RSpec.describe 'the doctor show page' do
         expect(page).to have_content("Grey Sloan Memorial Hospital")
         expect(page).to_not have_content("Turing Hospital")
     end
+   it 'has button to remove patient from doctors caseload' do
+      hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+      hospital2 = Hospital.create!(name: "Turing Hospital")
+      timmy = hospital2.doctors.create!(name: "Timmy", specialty: "Butts", university: "Turing University")
+      bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+      mcdreamy = hospital.doctors.create!(name: "Patrick Dempsy", specialty: "Brains", university: "Oregon State University")
+      patient1 = bailey.patients.create!(name:"Drew", age:27)
+      patient2 = bailey.patients.create!(name:"Daver", age:37)
+      patient3 = mcdreamy.patients.create!(name:"Patty", age:69)
+        visit "doctors/#{bailey.id}"
+        expect(page).to have_content("Drew")
+        expect(page).to have_content("Daver")
+
+      within"#patient_drew" do
+        click_button 'Create'
+      end
+
+      expect(page).to have_current_path("doctors/#{bailey.id}")
+      expect(page).to_not have_content("Drew")
+      expect(page).to have_content("Daver")
+    end
   end
