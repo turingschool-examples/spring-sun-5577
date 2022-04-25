c299af2466be0492f9e065e653fbc550c7f4ef00
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-25 11:36:18 -0600

    Refactor: change delete route to go to doctor-patients controller #destroy action

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
 
   resources :hospitals, only: [:show]
-  resources :doctors, only: [:show, :destroy]
+  # resources :doctors, only: [:show, :destroy]
 
   get '/patients', to: 'patients#index'
-  # get '/doctors/:id', to: 'doctors#show'
-  # delete '/doctors/:id', to: 'doctors#destroy'
+  get '/doctors/:id', to: 'doctors#show'
+  delete '/doctors/:id', to: 'doctor_patients#destroy'
 
   # get '/hospitals/:id', to: 'hospitals#show'
 end
