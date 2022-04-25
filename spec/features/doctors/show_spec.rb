9a20b954bfaad97394144212ce01b521728ce78a
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 11:30:49 -0400

    Refactor: Refactor routes in tests, all '.new' usages change to '.create'

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -3,19 +3,20 @@ require 'rails_helper'
 RSpec.describe 'Doctor Show Page', type: :feature do
 
   it "Shows an individual doctor's information" do
-    hospital_1 = Hospital.new(name: "Pro Wrestling General Hospital")
-    hospital_2 = Hospital.new(name: "New Japan Pro General")
-    doctor_1 = Doctor.new(name: "Buff Bagwell", specialty: "The Stuff", university: "Hard Knocks", hospital_id: hospital_1.id)
-    doctor_2 = Doctor.new(name: "Kidd Bandit", specialty: "Anime Surgery", university: "Game Changer University", hospital_id: hospital_2.id)
-    patient_1 = Patient.new(name: "Kenny Omega", age: 36)
-    patient_2 = Patient.new(name: "Edge", age: 43)
-    patient_3 = Patient.new(name: "Sting", age: 65)
-    DoctorsPatient.new(patient_id: patient_1.id, doctor_id: doctor_1.id)
-    DoctorsPatient.new(patient_id: patient_2.id, doctor_id: doctor_1.id)
-    DoctorsPatient.new(patient_id: patient_3.id, doctor_id: doctor_2.id)
-    DoctorsPatient.new(patient_id: patient_2.id, doctor_id: doctor_2.id)
+    hospital_1 = Hospital.create(name: "Pro Wrestling General Hospital")
+    hospital_2 = Hospital.create(name: "New Japan Pro General")
+    doctor_1 = Doctor.create(name: "Buff Bagwell", specialty: "The Stuff", university: "Hard Knocks", hospital_id: hospital_1.id)
+    doctor_2 = Doctor.create(name: "Kidd Bandit", specialty: "Anime Surgery", university: "Game Changer University", hospital_id: hospital_2.id)
+    patient_1 = Patient.create(name: "Kenny Omega", age: 36)
+    patient_2 = Patient.create(name: "Edge", age: 43)
+    patient_3 = Patient.create(name: "Sting", age: 65)
+    DoctorsPatient.create(patient_id: patient_1.id, doctor_id: doctor_1.id)
+    DoctorsPatient.create(patient_id: patient_2.id, doctor_id: doctor_1.id)
+    DoctorsPatient.create(patient_id: patient_3.id, doctor_id: doctor_2.id)
+    DoctorsPatient.create(patient_id: patient_2.id, doctor_id: doctor_2.id)
 
-    visit "/doctors/#{doctor_1.id}"
+
+    visit doctor_path(doctor_1.id)
 
     within "#doctor-info" do
       expect(page).to have_content(doctor_1.name)
@@ -34,7 +35,7 @@ RSpec.describe 'Doctor Show Page', type: :feature do
       expect(page).to_not have_content(patient_3.name)
     end
 
-    visit "/doctors/#{doctor_2.id}"
+    visit doctor_path(doctor_2.id)
 
     within "#doctor-info" do
       expect(page).to_not have_content(doctor_1.name)
