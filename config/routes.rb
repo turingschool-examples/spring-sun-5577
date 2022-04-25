ce4444a30be99a898105effee13f708dd8a68706
Alex Psitos <apsitos08@gmail.com>
2022-04-25 13:58:20 -0400

    Feat: add route for extension

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :doctors, only: [:show]
   resources :doctor_patients, only: [:destroy]
   resources :hospitals, only: [:show]
+  resources :patients, only: [:index]
 end
