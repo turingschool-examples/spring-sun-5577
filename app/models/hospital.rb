21515098e7420d7151ded72210a5fd2218a22941
Sherman Au <shermau13@gmail.com>
2022-04-25 11:18:02 -0600

    feat: .unique_universities

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -5,4 +5,8 @@ class Hospital < ApplicationRecord
   has_many :doctor_patients, through: :doctors
   #while it seems redundant it tossed me an error in my test when I removed the through association
   has_many :patients, through: :doctor_patients
+
+  def unique_universities
+    doctors.map { |doctor| doctor.university}.uniq
+  end
 end
