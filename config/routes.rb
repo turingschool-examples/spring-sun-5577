c9ba23dcdcf9c99f6a7f0af4658bbffd9afbbc7a
Sherman Au <shermau13@gmail.com>
2022-04-25 12:47:38 -0600

    refactor: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :doctors, only:[:show]
+
   resources :hospitals, only:[:show]
-  resources :doctor_patients, only:[:destroy]
+
+  resources :doctors, only:[:show] do
+    resources :doctor_patients, only:[:destroy]
+  end
 end
