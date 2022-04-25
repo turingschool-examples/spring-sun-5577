32a6b8dc8e7ca20689f4f348afd7185f56d3bfd8
Chris Romero <chrisleeromero1@gmail.com>
2022-04-25 10:17:03 -0600

    feat: create patients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_14_205253) do
+ActiveRecord::Schema.define(version: 2022_04_25_161320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,11 @@ ActiveRecord::Schema.define(version: 2021_06_14_205253) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
   end
 
   add_foreign_key "doctors", "hospitals"
