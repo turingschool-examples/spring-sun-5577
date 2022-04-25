532556b72eaf6fa7f60d5c5fca1648be988ef241
James Harkins <jh.harkins@protonmail.com>
2022-04-25 09:05:37 -0600

    ModelTest: create tests for Patient and DoctorPatient models

---
+++ spec/models/patient_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Patient, type: :model do
+  describe 'relationships' do
+    it { should have_many :doctor_patients }
+    it { should have_many(:doctors).through(:doctor_patients) }
+  end
+end
