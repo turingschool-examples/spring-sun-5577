10bee6f526b50e4d12768d069ef67dd746d378bb
Will Medders <wmedders@gmail.com>
2022-04-25 12:25:49 -0400

    Feat: hosp show displays name, number of doctors, and unique universities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hospitals/doctors/:id', to: 'hospitals_doctors#show'
+  get '/hospitals/:id', to: 'hospitals#show'
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe Hospital do
   describe 'relationships' do
     it { should have_many(:doctors) }
   end
+
+  describe 'instance methods' do
+    it '#doctor_count' do
+      hosp_1 = Hospital.create!(name: 'City Health')
+      doc_1 = hosp_1.doctors.create(name: 'Jim Pfeiffer', specialty: 'Pediatrics', university: 'Vanderbilt')
+      doc_2 = hosp_1.doctors.create(name: 'Ted Leftwich', specialty: 'Proctology', university: 'Emory')
+      doc_3 = hosp_1.doctors.create(name: 'Corbin Meikle', specialty: 'Brain Surgery', university: 'Emory')
+
+      expect(hosp_1.doctor_count).to eq(3)
+    end
+    
+    it '#unique_univs' do
+      hosp_1 = Hospital.create!(name: 'City Health')
+      doc_1 = hosp_1.doctors.create(name: 'Jim Pfeiffer', specialty: 'Pediatrics', university: 'Vanderbilt')
+      doc_2 = hosp_1.doctors.create(name: 'Ted Leftwich', specialty: 'Proctology', university: 'Emory')
+      doc_3 = hosp_1.doctors.create(name: 'Corbin Meikle', specialty: 'Brain Surgery', university: 'Emory')
+
+      expect(hosp_1.unique_univs.count).to eq(2)
+    end
+  end
 end
