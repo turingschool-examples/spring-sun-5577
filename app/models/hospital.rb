63b5e99a909e6b9efbf68ca32171382fdf01b4fc
Sherman Au <shermau13@gmail.com>
2022-04-25 10:00:26 -0600

    feat: HospitalModel validations and relationships

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,8 @@
 class Hospital < ApplicationRecord
+  validates :name, presence: true
+
   has_many :doctors
+  has_many :doctor_patients, through: :doctors
+  #while it seems redundant it tossed me an error in my test when I removed the through association
+  has_many :patients, through: :doctor_patients
 end
