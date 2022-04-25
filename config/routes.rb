0d0ce1b782888b9fb96a41155b7e6e34425e2354
Alicia Watt <alicia.watt.aw@gmail.com>
2022-04-25 10:05:41 -0600

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :hospitals, only: :show
+  resources :doctors, only: :show
 end
