751915a09c7bbbe6fcf3b3f02cf80b8754c8e627
Will Medders <wmedders@gmail.com>
2022-04-25 12:31:12 -0400

    FEATUREtest: doctors show has a button to remove a patient

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -30,4 +30,28 @@ RSpec.describe 'doctors show page' do
     expect(page).to have_no_content('Sports Medicine')
     expect(page).to have_no_content('35')
   end
+
+  it 'has a button to remove a patient' do
+    hosp_1 = Hospital.create!(name: 'City Health')
+    doc_1 = hosp_1.doctors.create(name: 'Jim Pfeiffer', specialty: 'Pediatrics', university: 'Vanderbilt')
+    doc_2 = hosp_1.doctors.create(name: 'Ted Leftwich', specialty: 'Proctology', university: 'Emory')
+    pat_1 = Patient.create!(name: 'Justin Hill', age: 39)
+    pat_2 = Patient.create!(name: 'Mark Gainey', age: 46)
+    pat_3 = Patient.create!(name: 'Liz Hagler', age: 35)
+    PatientDoctor.create!(doctor_id: doc_1.id, patient_id: pat_1.id)
+    PatientDoctor.create!(doctor_id: doc_1.id, patient_id: pat_2.id)
+    PatientDoctor.create!(doctor_id: doc_1.id, patient_id: pat_3.id)
+
+    visit "hospitals/doctors/#{doc_1.id}"
+    expect(page).to have_content('Justin Hill')
+    expect(page).to have_content('Mark Gainey')
+    expect(page).to have_content('Liz Hagler')
+
+    click_button "Remove Justin Hill"
+    expect(current_path).to eq("hospitals/doctors/#{doc_1.id}")
+
+    expect(page).to have_no_content('Justin Hill')
+    expect(page).to have_content('Mark Gainey')
+    expect(page).to have_content('Liz Hagler')
+  end
 end
