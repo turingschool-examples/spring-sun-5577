7b909edcc8cb0a9360ee92cf51c496d5c993f9d4
Johnny Bowman <johntbowman.production@gmail.com>
2022-04-25 10:45:29 -0600

    Test: Patient model lists patients from oldest to youngest

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Patient do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:age) }
   end
+
+  it "lists patients from oldest to youngest" do
+    mike = Patient.create!(name: "Mike Dao", age: 29)
+    dani = Patient.create!(name: "Dani Coleman", age: 28)
+    johnny = Patient.create!(name: "Johnny Bowman", age: 99)
+
+    expect(Patient.old_to_young).to eq([johnny, mike, dani])
+  end
 end
