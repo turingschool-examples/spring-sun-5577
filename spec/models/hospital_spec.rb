cbe593b0302ccab7d6fb7ce3f22f05d30df08569
Sherman Au <shermau13@gmail.com>
2022-04-25 11:17:55 -0600

    test: .unique_universities

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -10,4 +10,53 @@ RSpec.describe Hospital, type: :model do
     it { should have_many(:doctor_patients) }
     it { should have_many(:patients) }
   end
+
+  describe 'methods' do
+    before do
+      @hospital_2 = Hospital.create!(name: 'Sacred Heart Hospital')
+      @doctor_9 = Doctor.create!(
+        name: 'J.D.',
+        specialty: 'Human Connection',
+        university: 'Iowa State',
+        hospital_id: @hospital_2.id
+      )
+      @doctor_3 = Doctor.create!(
+        name: 'Cox',
+        specialty: 'Sarcasm',
+        university: 'University of Chicago',
+        hospital_id: @hospital_2.id
+      )
+      @doctor_6 = Doctor.create!(
+        name: 'Turk',
+        specialty: 'Neurogly',
+        university: 'NorthWestern',
+        hospital_id: @hospital_2.id
+      )
+
+      @hospital_1 = Hospital.create!(name: "Princeton - Plainsboro Teaching Hospital")
+      @doctor_4 = Doctor.create!(
+        name: 'House',
+        specialty: 'Neurogly',
+        university: 'Harvard',
+        hospital_id: @hospital_1.id
+      )
+      @doctor_2 = Doctor.create!(
+        name: 'Wilson',
+        specialty: 'Parasites',
+        university: 'Princeton',
+        hospital_id: @hospital_1.id
+      )
+      @doctor_8 = Doctor.create!(
+        name: 'Cuddy',
+        specialty: 'Pediactrics',
+        university: 'Princeton',
+        hospital_id: @hospital_1.id
+      )
+    end
+
+    it '.unique_universities' do
+      expected = (['Harvard','Princeton'])
+      expect(@hospital_1.unique_universities).to eq(expected)
+    end
+  end
 end
