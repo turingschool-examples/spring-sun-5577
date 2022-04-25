ac82bea5d95039f902b8a12817affbd881c2ecd8
Andrew Speth <speth.andrew@yahoo.com>
2022-04-25 10:19:16 -0600

    method test for hospital#doctor_count

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -17,5 +17,17 @@ RSpec.describe Hospital do
 
     expect(hospital_1.unique_universities).to eq(["SCCO", "SCUHS", "The Streets"])
     end
+    
+    it "#doctor_count" do
+    hospital_1 = Hospital.create!(name: "Rose Medical")
+    hospital_2 = Hospital.create!(name: "Fake Hospital")
+    doc_1 = hospital_1.doctors.create!(name: "Dr. Speth", specialty: "Optometry", university: "SCCO")
+    doc_2 = hospital_1.doctors.create!(name: "Dr. Dog", specialty: "Music", university: "The Streets")
+    doc_3 = hospital_1.doctors.create!(name: "Dr. LeGate", specialty: "Optometry", university: "SCCO")
+    doc_4 = hospital_1.doctors.create!(name: "Dr. Smith", specialty: "Chiropractic", university: "SCUHS")
+    doc_5 = hospital_2.doctors.create!(name: "Dr. Jan Itor", specialty: "Custodial Sciences", university: "Sacred Heart")
+
+    expect(hospital_1.doctor_count).to eq(4)
+    end
   end
 end
