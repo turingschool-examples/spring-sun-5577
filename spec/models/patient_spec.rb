3566d0663e14ad01f6ed6610098008976d8d99fa
Sherman Au <shermau13@gmail.com>
2022-04-25 12:54:59 -0600

    test: ::age_order for Patient

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,15 @@ describe Patient, type: :model do
     it {should have_many(:doctors)}
     it {should have_many(:doctor_patients)}
   end
+
+  describe 'methods' do
+    it '.age_order' do
+      @patient_3 = Patient.create!(name: "Jack", age: 54)
+      @patient_4 = Patient.create!(name: "Trey", age: 31)
+      @patient_2 = Patient.create!(name: "Scotty", age: 29)
+      @patient_6 = Patient.create!(name: "Chris", age: 30)
+
+      expect(Patient.age_order).to eq([@patient_3, @patient_4, @patient_6, @patient_2])
+    end
+  end
 end
