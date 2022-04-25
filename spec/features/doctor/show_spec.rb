bdba807c84b2b696762d011a5cdca5b0e6b5e396
reuben <creubend@gmail.com>
2022-04-25 09:45:28 -0600

    feat: Doctor show page has hosptial name

--- spec/features/doctor/show_spec.rb
+++ spec/features/doctor/show_spec.rb
@@ -2,11 +2,21 @@ require 'rails_helper'
 
 RSpec.describe "Doctor show page" do 
     it 'displays doctors info' do 
-    hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
-    bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
-    visit "/doctors/#{bailey.id}"
-    expect(page).to have_content("Bailey")
-    expect(page).to have_content("Surgery")
-    expect(page).to have_content("Stanford")
+        hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+        bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+        visit "/doctors/#{bailey.id}"
+        expect(page).to have_content("Bailey")
+        expect(page).to have_content("Surgery")
+        expect(page).to have_content("Stanford")
+    end
+
+    it 'displays hospital' do 
+        hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+        hospital2 = Hospital.create!(name: "The other hospital")
+        bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+        visit "/doctors/#{bailey.id}"
+
+        expect(page).to have_content('Grey Sloan')
+        expect(page).to_not have_content('other')
     end
 end
