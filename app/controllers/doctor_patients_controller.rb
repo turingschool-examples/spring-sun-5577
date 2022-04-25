a5ec9ed3244839f4fd120d9d8f77037f80de5e21
Alex Randolph <alexgeorgerandolph@gmail.com>
2022-04-25 11:47:58 -0600

    Refactor: refactor doctor_patients#destroy to redirect using resources style syntax

--- app/controllers/doctor_patients_controller.rb
+++ app/controllers/doctor_patients_controller.rb
@@ -2,9 +2,11 @@ class DoctorPatientsController < ApplicationController
 
   def destroy
     doctor = Doctor.find(params[:id])
-    DoctorPatient.find(params[:doc_patient_id]).destroy
+    doc_patient= DoctorPatient.find(params[:doc_patient_id])
+    doc_patient.destroy
     
-    redirect_to "/doctors/#{doctor.id}"
+    redirect_to(doctor_path)
+    # redirect_to "/doctors/#{doctor.id}"
   end 
 
 end 
