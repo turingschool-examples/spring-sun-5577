16a1f65e3d2ad129cc29a1768dc8d5843e130b53
Susan Boyd <sueboyd922@gmail.com>
2022-04-25 13:58:02 -0400

    Final refactors

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :doctors, only: [:show]
+  
+  # resources :doctors, only: [:show]
   resources :hospitals, only: [:show]
   resources :patients, only: [:index]
 
-  # resources :patient_doctors, only: [:destroy]
-  delete "/doctors/:doctor_id/patient_doctors/:id", to: "patient_doctors#destroy"
+  resources :doctors, only: [:show] do
+    resources :patient_doctors, only: [:destroy]
+  end
+  #I wasn't sure if this was the correct way to do this or how it could be done in resources or if it should have been what I have commented out
+
   # delete "/doctors/:doctor_id/patients/:id", to: "patient_doctors#destroy"
-  #I wasn't sure if this was the correct way to do this or how it could be done in resources or if it should have been what I have commented out above the handroll
+  # delete "/doctors/:doctor_id/patient_doctors/:id", to: "patient_doctors#destroy"
 end
