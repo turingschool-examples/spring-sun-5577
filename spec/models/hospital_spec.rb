0ef38897e5aed9f7926c5ed194b00ae75d642782
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-25 09:47:13 -0600

    Test: relationship tests for models, install orderly gem

--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -1,7 +1,8 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Hospital do
-  describe 'relationships' do
+RSpec.describe Hospital, type: :model do
+  describe "relationships" do
     it { should have_many(:doctors) }
+    it { should have_many(:patients).through(:doctors) }
   end
 end
