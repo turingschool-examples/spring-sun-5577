4d808eaea097ce89e80773d34947d689aea140c1
Zachary Imbriaco <zachary@imbriaco.com>
2022-04-25 12:50:19 -0400

    Feature: Doctors can remove patient associations from show page.

---
+++ app/controllers/doctors_patients_controller.rb
@@ -0,0 +1,10 @@
+class DoctorsPatientsController < ApplicationController
+
+  def delete
+    patient = Patient.find(params[:id])
+
+    patient.doctors.destroy(params[:doctor_id])
+
+    redirect_to doctor_path(params[:doctor_id])
+  end
+end
