0b9592562eb83eb1d6cd7b84d378015dc8b999b0
Joseph Senecal <jasenecal2@gmail.com>
2022-04-25 09:50:40 -0600

    Model: patient and patient_doctor models and updated relation tests

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Patient do
   describe 'relationships' do
 
     it { should have_many :patient_doctors }
-    it { should have_many :doctors, through: :patient_doctors }
-    
+    it { should have_many :doctors}
+
   end
 end
