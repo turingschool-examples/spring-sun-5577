f08bf75d138b8e699fb7feeb6889acac9edea42c
John Hennerich <john.hennerich@gmail.com>
2022-04-25 10:09:46 -0600

    Feat: happy with US 1

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -4,14 +4,15 @@ RSpec.describe 'Doctors Show Page', type: :feature do
     @hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
     @hospital1 = Hospital.create!(name: "Not my Hospital")
     @doc = @hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
-    @denny = bailey.patients.create!(name: "Denny Duquette", age: 39)
-    @john = bailey.patients.create!(name: "John", age: 49)
+    @doc1 = @hospital.doctors.create!(name: "Paul", specialty: "General Surgery", university: "Webster University")
+    @denny = @doc.patients.create!(name: "Denny Duquette", age: 39)
+    @john = @doc.patients.create!(name: "John", age: 49)
+    @ringo = @doc1.patients.create!(name: "Ringo", age: 70)
   end
 
   it 'Shows an individual doctor and all related attributes' do
     visit doctor_path(@doc.id)
 
-#      save_and_open_page
     within("#doc-#{@doc.id}") do
       expect(page).to have_content("Name: #{@doc.name}")
       expect(page).to have_content("Specialty: #{@doc.specialty}")
@@ -19,6 +20,9 @@ RSpec.describe 'Doctors Show Page', type: :feature do
       expect(page).to have_content(@hospital.name)
       expect(page).to_not have_content(@hospital1.name)
 
+      expect(page).to have_content("#{@denny.name}")
+      expect(page).to have_content("#{@john.name}")
+      expect(page).to_not have_content("#{@ringo.name}")
     end
   end
 end
