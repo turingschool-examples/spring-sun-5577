0caae5db665437aa885c12b4f307038b19d0cfae
reuben <creubend@gmail.com>
2022-04-25 10:17:55 -0600

    feat: hospital show page has the number of doctors

--- spec/features/hospital/show_spec.rb
+++ spec/features/hospital/show_spec.rb
@@ -17,4 +17,21 @@ RSpec.describe 'Hospital Show page' do
 
         expect(page).to have_content("Grey Sloan Memorial Hospital")
     end
+
+    it 'has the number of doctors' do 
+        hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+        bailey = hospital.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
+        bethany = hospital.doctors.create!(name: "Bethany", specialty: "General Surgery", university: "Stanford University")
+        frank = hospital.doctors.create!(name: "Frank", specialty: "Nurse", university: "Sydney School")
+        patient1 = Patient.create!(name: "Becky", age: 17)
+        patient2 = Patient.create!(name: "Bruce", age: 32)
+        patient3 = Patient.create!(name: "Rod", age: 43)
+        patient4 = Patient.create!(name: "Rick", age: 25)
+        PatientDoctor.create!(patient_id: patient1.id, doctor_id: bailey.id)
+        PatientDoctor.create!(patient_id: patient2.id, doctor_id: bailey.id)
+        PatientDoctor.create!(patient_id: patient3.id, doctor_id: bailey.id)
+        visit "/hospitals/#{hospital.id}"
+
+        expect(page).to have_content("Number of doctors: 3")
+    end
 end
