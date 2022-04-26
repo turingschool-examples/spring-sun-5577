e7638c120ba7cda3757310c42fc103f8fe62881f
Scott Sullivan <scottsullivanltd@gmail.com>
2022-04-26 00:13:49 -0600

    Refactor: remove if block from doctor show html

--- spec/features/doctors/show_spec.rb
+++ spec/features/doctors/show_spec.rb
@@ -55,32 +55,4 @@ RSpec.describe "Doctor Show Page" do
     expect(page).to have_content("Rod Smith")
     expect(page).to_not have_content("John Elway")
   end
-
-  it "has button to create a patient" do
-    hospital1 = Hospital.create!(name: "Grey Sloan Memorial Hospital")
-
-    doctor1 = hospital1.doctors.create!(name: "Miranda Bailey", specialty: "General Surgery", university: "Stanford University")
-
-    patient1 = Patient.create!(name: "John Elway", age: 66)
-    patient2 = Patient.create!(name: "Terrell Davis", age: 52)
-    patient3 = Patient.create!(name: "Rod Smith", age: 55)
-
-    DoctorPatient.create!(doctor: doctor1, patient: patient1)
-    DoctorPatient.create!(doctor: doctor1, patient: patient2)
-    DoctorPatient.create!(doctor: doctor1, patient: patient3)
-
-    visit doctor_path(doctor1.id)
-
-    expect(page).to have_button("Delete John Elway")
-
-    expect(page).to have_content("John Elway")
-    expect(page).to have_content("Terrell Davis")
-    expect(page).to have_content("Rod Smith")
-
-    click_button "Delete John Elway"
-
-    expect(page).to have_content("Terrell Davis")
-    expect(page).to have_content("Rod Smith")
-    expect(page).to_not have_content("John Elway")
-  end
 end
